2f3c0ee809134bac79782b19aa5322728839d9b3
solegdec <86257609+solegdec@users.noreply.github.com>
2021-07-29 17:19:44 -0300

    subi mercadoLiebre version final

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const port = 3030
+const path = require('path')
+
+
+
+app.get('/', function(request, response) {
+    response.sendFile(path.join(__dirname, 'views/index.html'))
+})
+app.get('/login', function(request, response) {
+    response.sendFile(path.join(__dirname, 'views/login.html'))
+})
+app.get('/registro', function(request, response) {
+    response.sendFile(path.join(__dirname, 'views/registro.html'))
+})
+
+app.use(express.static('public'))
+app.use(express.static('views'))
+
+app.get('*', function(request, response) {
+    response.send('NOT FOUND', 404)
+})
+
+app.listen(port, () => {
+    console.log('La app esta funcionado en http://localhost:' + port)
+})
+
